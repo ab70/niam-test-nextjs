89c98f6c797824960eb3c06803831f0cc3ce7d8d
ab70 <nurulabrar2369@gmail.com>
2023-05-15 16:51:26 +0600

    llo

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
+import axios from "axios"
+const formInitial = {
+    acts: "",
+    host: "",
+    ipAd: "",
+    json_files: "",
+    json_hash: "",
+    json_params: ""
+}
+
+const URL = "";
 
 export default function Test() {
+    const [formInputs, setFormInputs] = useState(formInitial)
+
+    //on input change track changes (signIn)form
+    const onSignInChange = (e) => {
+        setFormInputs({ ...formInputs, [e.target.name]: e.target.value })
+        console.log(formInputs);
+    }
+
+    const submitForm = async (data) => {
+        try {
+            
+            let submitData = await axios.post(`${URL}`, data, { withCredentials: true })
+        } catch (err) {
+            console.log("Can't submit");
+        }
+    }
+    const postForm = async ()=>{
+        let resReturn = await submitForm(formInputs)
+    }
     return (
         <>
             <div className="row m-4">
@@ -14,8 +45,6 @@ export default function Test() {
                         <div className="card-body">
                             <div className="live-preview">
                                 <div className="row gy-4">
-
-
                                     {/*end col*/}
                                     <div className="col-xxl-3 col-md-6">
                                         <div>
@@ -28,6 +57,7 @@ export default function Test() {
                                                 className="form-control"
                                                 id="placeholderInput"
                                                 placeholder="acts"
+                                                onChange={(e) => onSignInChange(e)}
                                             />
                                         </div>
                                     </div>
@@ -43,6 +73,7 @@ export default function Test() {
                                                 className="form-control"
                                                 id="placeholderInput"
                                                 placeholder="Host"
+                                                onChange={(e) => onSignInChange(e)}
                                             />
                                         </div>
                                     </div>
@@ -58,6 +89,7 @@ export default function Test() {
                                                 className="form-control"
                                                 id="placeholderInput"
                                                 placeholder="ipAd"
+                                                onChange={(e) => onSignInChange(e)}
                                             />
                                         </div>
                                     </div>
@@ -73,12 +105,47 @@ export default function Test() {
                                                 className="form-control"
                                                 id="placeholderInput"
                                                 placeholder="json_files"
+                                                onChange={(e) => onSignInChange(e)}
+                                            />
+                                        </div>
+                                    </div>
+                                    {/*end col*/}
+                                    <div className="col-xxl-3 col-md-6">
+                                        <div>
+                                            <label htmlFor="placeholderInput" className="form-label">
+                                                Json hash
+                                            </label>
+                                            <input
+                                                name="json_hash"
+                                                type="text"
+                                                className="form-control"
+                                                id="placeholderInput"
+                                                placeholder="json_hash"
+                                                onChange={(e) => onSignInChange(e)}
+                                            />
+                                        </div>
+                                    </div>
+                                    {/*end col*/}
+                                    <div className="col-xxl-3 col-md-6">
+                                        <div>
+                                            <label htmlFor="placeholderInput" className="form-label">
+                                                Json params
+                                            </label>
+                                            <input
+                                                name="json_params"
+                                                type="text"
+                                                className="form-control"
+                                                id="placeholderInput"
+                                                placeholder="json_files"
+                                                onChange={(e) => onSignInChange(e)}
                                             />
                                         </div>
                                     </div>
 
 
+
                                 </div>
+                                <button type="button" className="btn btn-primary btn-animation waves-effect waves-light mt-3" onClick={postForm} data-text="Primary"><span>Submit Data</span></button>
                             </div>
                         </div>
                     </div>
