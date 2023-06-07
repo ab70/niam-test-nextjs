b7127b3585dc01d7899c6abea28b88115ddd796a
ab70 <nurulabrar2369@gmail.com>
2023-06-07 10:34:03 +0600

    pushed data

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -11,9 +11,11 @@ const formInitial = {
     params: ""
 }
 
-const URL = "http://localhost:4000/api/test";
+const URL = "http://localhost:8080/api/v1/niam";
 
 export default function Test() {
+    const [responseData, setResponseData] = useState(null);
+
     const [formInputs, setFormInputs] = useState(formInitial)
 
     //on input change track changes (signIn)form
@@ -35,23 +37,17 @@ export default function Test() {
                         path: data.path,
                         params: data.params
                     },
-                    {
-                        id: data.id,
-                        action: data.action,
-                        path: data.path,
-                        params: data.params
-                    }
+
                 ]
             }
-            let postData = await axios.post(`${URL}`,JSON.stringify(submitData) , {
-                
-            });
+            let postData = await axios.post(`${URL}`, JSON.stringify(submitData));
         } catch (err) {
             console.log("Can't submit");
         }
     }
     const postForm = async () => {
-        let resReturn = await submitForm(formInputs)
+        let resReturn = await submitForm(formInputs);
+        setResponseData(resReturn.data);
     }
     return (
         <>
@@ -59,7 +55,7 @@ export default function Test() {
                 <div className="col-lg-12">
                     <div className="card">
                         <div className="card-header align-items-center d-flex">
-                            <h4 className="card-title mb-0 flex-grow-1">Input Example</h4>
+                            <h4 className="card-title mb-0 flex-grow-1">Host info</h4>
                         </div>
                         {/* end card header */}
                         <div className="card-body">
@@ -71,16 +67,25 @@ export default function Test() {
                                             <label htmlFor="placeholderInput" className="form-label">
                                                 ACts
                                             </label>
-                                            <input
+                                            <select
                                                 name="acts"
-                                                type="text"
                                                 className="form-control"
                                                 id="placeholderInput"
-                                                placeholder="acts"
                                                 onChange={(e) => onSignInChange(e)}
-                                            />
+                                            >
+                                                <option value="">Select an action</option>
+                                                <option value="NIAM_register">NIAM_register</option>
+                                                <option value="NIAM_deleteNode">NIAM_deleteNode</option>
+                                                <option value="NIAM_updateFiles">NIAM_updateFiles</option>
+                                                <option value="NIAM_verifyAgent">NIAM_verifyAgent</option>
+                                                <option value="NIAM_verifyFileLink">NIAM_verifyFileLink</option>
+                                                <option value="NIAM_triggerUpdateHash">NIAM_triggerUpdateHash</option>
+                                                <option value="NIAM_getActionList">NIAM_getActionList</option>
+                                                <option value="NIAM_Action">NIAM_Action</option>
+                                            </select>
                                         </div>
                                     </div>
+
                                     {/*end col*/}
                                     <div className="col-xxl-3 col-md-6">
                                         <div>
@@ -113,6 +118,20 @@ export default function Test() {
                                             />
                                         </div>
                                     </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card">
+                        <div className="card-header align-items-center d-flex">
+                            <h4 className="card-title mb-0 flex-grow-1">Json_Files info</h4>
+                        </div>
+                        {/* end card header */}
+                        <div className="card-body">
+                            <div className="live-preview">
+                                <div className="row gy-4">
+
                                     {/*end col*/}
                                     <div className="col-xxl-3 col-md-6">
                                         <div>
@@ -177,17 +196,28 @@ export default function Test() {
                                             />
                                         </div>
                                     </div>
-
-
-
                                 </div>
                                 <button type="button" className="btn btn-primary btn-animation waves-effect waves-light mt-3" onClick={postForm} data-text="Submit"><span>Submit Data</span></button>
                             </div>
                         </div>
                     </div>
+
+                    <div className="card">
+                        <div className="card-header align-items-center d-flex">
+                            <h4 className="card-title mb-0 flex-grow-1">Json Response</h4>
+                        </div>
+                        {/* end card header */}
+                        <div className="card-body">
+                            <div className="live-preview">
+                                {responseData && (
+                                    <pre>{JSON.stringify(responseData, null, 2)}</pre>
+                                )}
+                                {/* here the response will be shown */}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-
         </>
     )
 }
